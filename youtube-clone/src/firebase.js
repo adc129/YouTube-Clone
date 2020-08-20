af9eb7403623cec440a2ea3bff0696adc43cd16c
adc129 <60369328+adc129@users.noreply.github.com>
2020-08-20 17:53:40 -0500

    header and sidebar

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDNtkK33Fn015igWGwTgGgjdJBxqPGtPH0",
+  authDomain: "clone-18232.firebaseapp.com",
+  databaseURL: "https://clone-18232.firebaseio.com",
+  projectId: "clone-18232",
+  storageBucket: "clone-18232.appspot.com",
+  messagingSenderId: "773267580109",
+  appId: "1:773267580109:web:1f0fd77110c32c8ebb3584",
+  measurementId: "G-C1NF8ERB8L"
+};
