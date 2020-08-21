6d8895664ecc3a696b221353efc7c4a6d7405996
adc129 <60369328+adc129@users.noreply.github.com>
2020-08-20 20:45:09 -0500

    videos

--- youtube-clone/src/RecommendedVideos.js
+++ youtube-clone/src/RecommendedVideos.js
@@ -1,9 +1,70 @@
 import React from 'react'
+import VideoCard from "./VideoCard";
 import "./RecommendedVideos.css"
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
+    <h2>Recommended</h2>
+    <div className="recommendedVideos__videos">
+    <VideoCard
+    title="test title"
+    views="1.3m views"
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""
+    />
+    <VideoCard
+    title=""
+    views=""
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""/>
+    <VideoCard
+    title=""
+    views=""
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""/>
+    <VideoCard
+    title=""
+    views=""
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""/>
+    <VideoCard
+    title=""
+    views=""
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""/>
+    <VideoCard
+    title=""
+    views=""
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""/>
+    <VideoCard
+    title=""
+    views=""
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""/>
+    <VideoCard
+    title=""
+    views=""
+    timestamp=""
+    channelImage=""
+    channel=""
+    image=""/>
+    </div>
     </div>
 
   )
--- youtube-clone/src/SidebarRow.js
+++ youtube-clone/src/SidebarRow.js
@@ -4,6 +4,7 @@ import './SidebarRow.css';
 
 function SidebarRow({ selected, Icon, title}) {
 return (
+  //eslint-disable-next-line
 <div className={'sidebarRow ${selected && "selected"}'}>
 <Icon className="sidebarRow__icon"/>
 <h2 className="sidebarRow__title">{title}</h2>
